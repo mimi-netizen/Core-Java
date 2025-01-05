00dac6bc6390d65d83ddd2baa6c1d19eb05e4634
Celyne <dianakydd2@gmail.com>
2025-01-05 10:04:58 +0300

    Inheritance & Abstraction

---
+++ inheritance/Class2.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class Class2 extends Class1 {
+
+	
+	public void doSubtraction() {
+		System.out.println("Subtraction is " + (num1 - num2));
+	}
+}
+
---
+++ inheritance/Rectangle.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class Rectangle extends Polygon{
+	
+	public void calcArea() {
+		System.out.println("Area of Rectangle is " + (base*height));
+	}
+}
+
