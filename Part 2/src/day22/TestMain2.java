5c0add7058f75b0dd3ce3dd674a12579a10df520
Celyne <dianakydd2@gmail.com>
2025-01-10 08:36:34 +0300

    core java

---
+++ Part 2/src/day22/TestMain2.java
@@ -0,0 +1,14 @@
+package day22;
+
+public class TestMain2 {
+	
+	public static void main(String[] args) {
+		
+		Test object = new Test();
+		int a = object.x;
+		System.out.println(a);
+		object.method();
+
+	}
+
+}
